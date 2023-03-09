52a6447fbfe99404f2bf26f40ee8bf9a0ef3399c
oliver-peoples <oliver.kirkpatrick@rmit.edu.au>
2023-03-09 16:35:38 +1100

    stuff

---
+++ tem_mode_pattern_generation.py
@@ -0,0 +1,10 @@
+from scipy.special import hermite as physicistsHermite
+import math
+
+print(physicistsHermite(3)(1))
+
+def temModeFnXY(m: int, n: int, x: float, y: float, I_0: float = 1, w_0: float = 1, w: float = 1) -> float:
+    
+    return I_0 * (w_0 / w)**2 * (physicistsHermite(m)((2**0.5 * x)/w) * math.exp(-x**2 / -w**2))**2 * (physicistsHermite(n)((2**0.5 * y)/w) * math.exp(-y**2 / -w**2))**2
+
+print(temModeFnXY(2, 1, 0.4, 0.3))
