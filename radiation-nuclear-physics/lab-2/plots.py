30f2aac30cd035280123cc892fa21d04fe5f2605
Oliver Kirkpatrick <59456356+oliver-peoples@users.noreply.github.com>
2023-04-05 11:20:49 +1000

    stuff

---
+++ radiation-nuclear-physics/lab-2/plots.py
@@ -0,0 +1,28 @@
+from loadAsciiSpectrum import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
+import os
+
+def backgroundRadiation() -> None:
+    
+    bin_counts, mca_coefficients = loadAsciiSpectrum(os.path.join('data','calibration_spectrum_bg.Spe'))
+    
+    energy_counts = bin_counts.astype(dtype=np.float64)
+    
+    energy_counts[:,0] = mca_coefficients[0] * (energy_counts[:,0] + 1) + mca_coefficients[1] * energy_counts[:,0] ** 2 + mca_coefficients[2] * energy_counts[:,0] ** 3
+    
+    plt.plot(energy_counts[:,0], energy_counts[:,1])
+    plt.show()
+    
+def mysteryIsotope() -> None:
+    
+    for mystery_idx in range(1,6)
+
+def main() -> None:
+    
+    backgroundRadiation()
+
+if __name__ == '__main__':
+    
+    main()
