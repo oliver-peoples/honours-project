fc517bfd9a3877733edb9717922d6a8670c426df
oliver-peoples <oliver.kirkpatrick@rmit.edu.au>
2023-07-18 18:08:43 +1000

    stuff

--- gauss_laguerre.py
+++ gauss_laguerre.py
@@ -8,10 +8,10 @@ matplotlib.rcParams['text.usetex'] = True
 
 grid_x = 1750
 grid_y = 1750
-waists = 20
+waists = 3
 
-order = 37
-index = 40
+order = 3
+index = 1
 waist = 1.
 I_0 = 1.
 
@@ -60,8 +60,9 @@ phi_meshgrid = np.arctan2(y_meshgrid, x_meshgrid)
 rho_meshgrid = rho(r_meshgrid, waist)
 gen_laguerre = GenLaguerre(order, index)
 gl_tem = I_0 * (rho_meshgrid**index) * (gen_laguerre(rho_meshgrid)**2) * (np.cos(index * phi_meshgrid)**2) * np.exp(-rho_meshgrid)
+gl_tem /= np.max(gl_tem)
 
-plt.title(r'$\mathrm{TEM}_{pl},\;p=' + str(order) + r',\;l=' + str(index) + r'$', fontsize=28)
+plt.title(r'$\mathrm{TEM}_{pl},\;p=' + str(order) + r',\;l=' + str(index) + r'$', fontsize=28, pad=10)
 plt.pcolormesh(x_meshgrid / waist, y_meshgrid / waist, gl_tem, cmap=parula)
 plt.xlabel(r"$x/w$", fontsize=24)
 plt.ylabel(r"$y/w$", fontsize=24)
@@ -69,9 +70,9 @@ plt.xticks(fontsize=22)
 plt.yticks(fontsize=22)
 cbar = plt.colorbar(pad=0.01)
 cbar.ax.tick_params(labelsize=18)
-cbar.set_label(r"$I_{pl}\left(\rho,\phi\right)$", fontsize=24, rotation=-90, labelpad=28)
+cbar.set_label(r"$I_{pl}\left(\rho,\phi\right)/I_{max}$", fontsize=24, rotation=-90, labelpad=28)
 # cbar.set_ticks
 plt.gca().set_aspect(1)
 plt.tight_layout()
-plt.savefig(f'gl-modes/p_{order}_l_{index}_i_pl.png', dpi=600, bbox_inches='tight')
+plt.savefig(f'gl-modes/p_{order}_l_{index}_i_pl.png', dpi=400, bbox_inches='tight')
 plt.close()
